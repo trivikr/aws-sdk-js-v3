d5418b9ec30f2b817fb41e7d9c0491252954375f
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-07-08 04:21:07 +0000

    test: presign with options

--- packages/middleware-sdk-transcribe-streaming/src/signer.spec.ts
+++ packages/middleware-sdk-transcribe-streaming/src/signer.spec.ts
@@ -21,18 +21,31 @@ describe("signer", () => {
   });
 
   describe("presign", () => {
-    it("without options", async () => {
-      const request = { headers, body, method };
-      const sigV4 = new SignatureV4({ signer: { sign, presign } as any });
-      const result = await sigV4.presign(request as any);
+    const request = { headers, body, method };
+    const sigV4 = new SignatureV4({ signer: { sign, presign } as any });
 
+    const expectPresignArgs = (result: any) => {
       expect(result).toStrictEqual(mockPresignedRequest);
       expect(sign).not.toHaveBeenCalled();
       expect(presign).toHaveBeenCalledTimes(1);
+    };
+
+    it("without options", async () => {
+      const result = await sigV4.presign(request as any);
+
+      expectPresignArgs(result);
       expect(presign).toHaveBeenCalledWith(request, {});
     });
 
-    it("with options", () => {});
+    it("with options", async () => {
+      const options = {
+        expiresIn: 300
+      };
+      const result = await sigV4.presign(request as any, options);
+
+      expectPresignArgs(result);
+      expect(presign).toHaveBeenCalledWith(request, options);
+    });
   });
 
   describe("sign", () => {
