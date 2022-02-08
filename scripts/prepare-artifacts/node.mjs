3cffcb631df1b8349949116b7287b6179c98433e
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-02-08 22:36:26 +0000

    chore: add script to update package version

--- scripts/prepare-artifacts/node.mjs
+++ scripts/prepare-artifacts/node.mjs
@@ -1,4 +1,9 @@
 import { getWorkspacePaths } from "../update-versions/getWorkspacePaths.mjs";
+import { updatePackageJsonVersion } from "./updatePackageJsonVersion.mjs";
 
 const workspacePaths = getWorkspacePaths();
-console.log({ workspacePaths });
+
+// TODO: Bumping version will be done by lerna in prod version of this test.
+// We're bumping just to have same version for all packages.
+const version = process.argv[process.argv.length - 1].replace("v", "");
+await updatePackageJsonVersion(workspacePaths, version);
