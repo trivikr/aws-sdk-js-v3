ba99738be78f29f935628136644fe11471e0c07d
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-02-08 23:07:42 +0000

    chore(scripts): addBuildMetadataVersionSuffix

--- scripts/prepare-artifacts/node.mjs
+++ scripts/prepare-artifacts/node.mjs
@@ -1,9 +1,11 @@
 import { getWorkspacePaths } from "../update-versions/getWorkspacePaths.mjs";
+import { addBuildMetadataVersionSuffix } from "./addBuildMetadataVersionSuffix.mjs";
 import { addPreReleaseVersionSuffix } from "./addPreReleaseVersionSuffix.mjs";
 import { updatePackageJsonVersion } from "./updatePackageJsonVersion.mjs";
 
 const workspacePaths = getWorkspacePaths();
 const prereleaseTag = "test";
+const buildMetadata = "node-cjs";
 
 // TODO: Bumping version will be done by lerna in prod version of this test script.
 // We're bumping just to have same version for all packages.
@@ -13,3 +15,5 @@ await updatePackageJsonVersion(workspacePaths, version);
 // TODO: Adding prerelease version will not be required in prod version of this test script.
 // The prerelease version is being added to that it doesn't interfere with stable versions.
 await addPreReleaseVersionSuffix(workspacePaths, prereleaseTag);
+
+await addBuildMetadataVersionSuffix(workspacePaths, buildMetadata);
