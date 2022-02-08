7b6df639628b2c710f7cc09e94b9cf724826ccf9
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-02-08 23:04:45 +0000

    chore(scripts): addPreReleaseVersionSuffix

--- scripts/prepare-artifacts/node.mjs
+++ scripts/prepare-artifacts/node.mjs
@@ -1,9 +1,15 @@
 import { getWorkspacePaths } from "../update-versions/getWorkspacePaths.mjs";
+import { addPreReleaseVersionSuffix } from "./addPreReleaseVersionSuffix.mjs";
 import { updatePackageJsonVersion } from "./updatePackageJsonVersion.mjs";
 
 const workspacePaths = getWorkspacePaths();
+const prereleaseTag = "test";
 
-// TODO: Bumping version will be done by lerna in prod version of this test.
+// TODO: Bumping version will be done by lerna in prod version of this test script.
 // We're bumping just to have same version for all packages.
 const version = process.argv[process.argv.length - 1].replace("v", "");
 await updatePackageJsonVersion(workspacePaths, version);
+
+// TODO: Adding prerelease version will not be required in prod version of this test script.
+// The prerelease version is being added to that it doesn't interfere with stable versions.
+await addPreReleaseVersionSuffix(workspacePaths, prereleaseTag);
