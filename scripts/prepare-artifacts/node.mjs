12a5d4499083cc5cf2e8ea9363f6210d3fff1e34
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2022-02-09 10:41:12 -0800

    feat(script): add script prepare:artifacts:node (#279)
    Co-authored-by: AllanZhengYP <zheallan@amazon.com>

---
+++ scripts/prepare-artifacts/node.mjs
@@ -0,0 +1,36 @@
+import { getDepToCurrentVersionHash } from "../update-versions/getDepToCurrentVersionHash.mjs";
+import { getWorkspacePaths } from "../update-versions/getWorkspacePaths.mjs";
+import { updateVersions } from "../update-versions/updateVersions.mjs";
+import { addBuildMetadataVersionSuffix } from "./addBuildMetadataVersionSuffix.mjs";
+import { addPreReleaseVersionSuffix } from "./addPreReleaseVersionSuffix.mjs";
+import { deleteFilesWithExtension } from "./deleteFilesWithExtension.mjs";
+import { deleteNotNodeDeps } from "./deleteNotNodeDeps.mjs";
+import { deleteNotNodeEntriesInPackageJson } from "./deleteNotNodeEntriesInPackageJson.mjs";
+import { updateFilesInPackageJson } from "./updateFilesInPackageJson.mjs";
+import { updatePackageJsonVersion } from "./updatePackageJsonVersion.mjs";
+
+const workspacePaths = getWorkspacePaths();
+const prereleaseTag = "test";
+const buildMetadata = "node-cjs";
+
+// TODO: Bumping version will be done by lerna in prod version of this test script.
+// We're bumping just to have same version for all packages.
+const version = process.argv[process.argv.length - 1].replace("v", "");
+await updatePackageJsonVersion(workspacePaths, version);
+
+// TODO: Adding prerelease version will not be required in prod version of this test script.
+// The prerelease version is being added to that it doesn't interfere with stable versions.
+await addPreReleaseVersionSuffix(workspacePaths, prereleaseTag);
+
+await addBuildMetadataVersionSuffix(workspacePaths, buildMetadata);
+
+updateVersions(getDepToCurrentVersionHash());
+
+await deleteNotNodeEntriesInPackageJson(workspacePaths);
+
+await deleteFilesWithExtension(workspacePaths, "dist-cjs", "*.browser.js");
+await deleteFilesWithExtension(workspacePaths, "dist-cjs", "*.native.js");
+
+await updateFilesInPackageJson(workspacePaths, ["dist-cjs"]);
+
+await deleteNotNodeDeps(workspacePaths);
