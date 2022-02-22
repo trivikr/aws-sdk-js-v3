3c09fab8b02c94abaa8011d008472f3d68c798fc
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-02-22 15:33:23 +0000

    chore: remove explicitly setting package.json version

--- scripts/prepare-artifacts/cjs.mjs
+++ scripts/prepare-artifacts/cjs.mjs
@@ -6,15 +6,9 @@ import { deleteFilesWithExtension } from "./deleteFilesWithExtension.mjs";
 import { deleteNotNodeDeps } from "./deleteNotNodeDeps.mjs";
 import { deleteNotNodeEntriesInPackageJson } from "./deleteNotNodeEntriesInPackageJson.mjs";
 import { updateFilesInPackageJson } from "./updateFilesInPackageJson.mjs";
-import { updatePackageJsonVersion } from "./updatePackageJsonVersion.mjs";
 
 const workspacePaths = getWorkspacePaths();
-const buildMetadata = "node-cjs";
-
-// TODO: Bumping version will be done by lerna in prod version of this test script.
-// We're bumping just to have same version for all packages.
-const version = process.argv[process.argv.length - 1].replace("v", "");
-await updatePackageJsonVersion(workspacePaths, version);
+const buildMetadata = "cjs";
 
 await addBuildMetadataVersionSuffix(workspacePaths, buildMetadata);
 
