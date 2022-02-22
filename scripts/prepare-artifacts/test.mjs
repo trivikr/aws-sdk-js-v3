f67aee070a70bfddecff387c0ef76bfc5a95c0df
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-02-22 20:38:51 +0000

    chore: revert temporary file created for original publish

--- scripts/prepare-artifacts/test.mjs
+++
@@ -1,5 +0,0 @@
-import { getWorkspacePaths } from "../utils/getWorkspacePaths.mjs";
-import { renameOrgInPackageName } from "./renameOrgInPackageName.mjs";
-
-const workspacePaths = getWorkspacePaths();
-await renameOrgInPackageName(workspacePaths, "@trivikr-test");
