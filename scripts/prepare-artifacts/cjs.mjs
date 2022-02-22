4b7b16ad841e285c75c1d3c6e0a93d552105a236
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-02-22 15:30:52 +0000

    chore: remove pre-release version suffix

--- scripts/prepare-artifacts/cjs.mjs
+++ scripts/prepare-artifacts/cjs.mjs
@@ -2,7 +2,6 @@ import { getDepToCurrentVersionHash } from "../update-versions/getDepToCurrentVe
 import { getWorkspacePaths } from "../update-versions/getWorkspacePaths.mjs";
 import { updateVersions } from "../update-versions/updateVersions.mjs";
 import { addBuildMetadataVersionSuffix } from "./addBuildMetadataVersionSuffix.mjs";
-import { addPreReleaseVersionSuffix } from "./addPreReleaseVersionSuffix.mjs";
 import { deleteFilesWithExtension } from "./deleteFilesWithExtension.mjs";
 import { deleteNotNodeDeps } from "./deleteNotNodeDeps.mjs";
 import { deleteNotNodeEntriesInPackageJson } from "./deleteNotNodeEntriesInPackageJson.mjs";
@@ -10,17 +9,12 @@ import { updateFilesInPackageJson } from "./updateFilesInPackageJson.mjs";
 import { updatePackageJsonVersion } from "./updatePackageJsonVersion.mjs";
 
 const workspacePaths = getWorkspacePaths();
-const prereleaseTag = "test";
 const buildMetadata = "node-cjs";
 
 // TODO: Bumping version will be done by lerna in prod version of this test script.
 // We're bumping just to have same version for all packages.
 const version = process.argv[process.argv.length - 1].replace("v", "");
 await updatePackageJsonVersion(workspacePaths, version);
-
-// TODO: Adding prerelease version will not be required in prod version of this test script.
-// The prerelease version is being added to that it doesn't interfere with stable versions.
-await addPreReleaseVersionSuffix(workspacePaths, prereleaseTag);
 
 await addBuildMetadataVersionSuffix(workspacePaths, buildMetadata);
 
