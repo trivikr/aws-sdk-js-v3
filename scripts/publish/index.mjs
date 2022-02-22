e68ae88597302882b7a9f7b108140497e5ee6cf3
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-02-22 16:07:42 +0000

    fix: read tag as build metadata version

--- scripts/publish/index.mjs
+++ scripts/publish/index.mjs
@@ -1,5 +1,6 @@
 import { exec } from "child_process";
-import { basename } from "path";
+import { readFile } from "fs/promises";
+import { basename, join } from "path";
 import { promisify } from "util";
 
 import { getWorkspacePaths } from "../utils/getWorkspacePaths.mjs";
@@ -8,8 +9,12 @@ const execPromise = promisify(exec);
 const workspacePaths = getWorkspacePaths().filter((workspacePath) => !basename(workspacePath).startsWith("aws-"));
 
 for (const workspacePath of workspacePaths) {
+  const packageJsonPath = join(workspacePath, "package.json");
+  const packageJsonBuffer = await readFile(packageJsonPath);
+  const { version } = JSON.parse(packageJsonBuffer.toString());
+  const tag = version.substring(version.indexOf("+") + 1);
+
   // https://docs.npmjs.com/adding-dist-tags-to-packages
-  const npmPublishCommand = `npm publish --tag cjs`;
-  console.log({ npmPublishCommand, cwd: workspacePath });
+  const npmPublishCommand = `npm publish${tag ? ` --tag ${tag}` : ``}`;
   await execPromise(npmPublishCommand, { cwd: workspacePath });
 }
