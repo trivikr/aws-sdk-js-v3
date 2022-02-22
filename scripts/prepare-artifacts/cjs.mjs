a65d79514b81bd1511517197fbdb4ec99635fff0
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2022-02-22 07:53:26 -0800

    feat(script): add script prepare:artifacts:cjs (#290)
    Co-authored-by: AllanZhengYP <zheallan@amazon.com>

---
+++ scripts/prepare-artifacts/cjs.mjs
@@ -0,0 +1,31 @@
+import { getDepToCurrentVersionHash } from "../update-versions/getDepToCurrentVersionHash.mjs";
+import { updateVersions } from "../update-versions/updateVersions.mjs";
+import { getWorkspacePaths } from "../utils/getWorkspacePaths.mjs";
+import { addBuildMetadataVersionSuffix } from "./addBuildMetadataVersionSuffix.mjs";
+import { deleteFilesWithExtension } from "./deleteFilesWithExtension.mjs";
+import { deleteNotNodeDeps } from "./deleteNotNodeDeps.mjs";
+import { deleteNotNodeEntriesInPackageJson } from "./deleteNotNodeEntriesInPackageJson.mjs";
+import { renameOrgInPackageName } from "./renameOrgInPackageName.mjs";
+import { updateFilesInPackageJson } from "./updateFilesInPackageJson.mjs";
+
+// In release automation, the steps need to be run for all workspace just once.
+// After that the steps needs to be only for the packages which need to be published.
+const workspacePaths = getWorkspacePaths();
+const buildMetadata = "cjs";
+
+await addBuildMetadataVersionSuffix(workspacePaths, buildMetadata);
+
+updateVersions(getDepToCurrentVersionHash());
+
+await deleteNotNodeEntriesInPackageJson(workspacePaths);
+
+await deleteFilesWithExtension(workspacePaths, "dist-cjs", "*.browser.js");
+await deleteFilesWithExtension(workspacePaths, "dist-cjs", "*.native.js");
+
+await updateFilesInPackageJson(workspacePaths, ["dist-cjs"]);
+
+await deleteNotNodeDeps(workspacePaths);
+
+// Renaming org in package name is not needed in production script.
+// This is added for testing published packages with `@trivikr-test` org.
+await renameOrgInPackageName(workspacePaths, "@trivikr-test");
