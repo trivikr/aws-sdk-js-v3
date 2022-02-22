fac0aa1e58e49c1d65161019d94010bb43317dec
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-02-22 21:06:19 +0000

    fix(scripts): add prerelease instead of build metadata

--- scripts/prepare-artifacts/addPreReleaseVersionSuffix.mjs
+++ scripts/prepare-artifacts/addPreReleaseVersionSuffix.mjs
@@ -1,12 +1,12 @@
 import { readFile, writeFile } from "fs/promises";
 import { join } from "path";
 
-export const addBuildMetadataVersionSuffix = async (workspacePaths, buildMetadata) => {
+export const addPreReleaseVersionSuffix = async (workspacePaths, prerelease) => {
   for (const workspacePath of workspacePaths) {
     const packageJsonPath = join(workspacePath, "package.json");
     const packageJsonBuffer = await readFile(packageJsonPath);
     const packageJson = JSON.parse(packageJsonBuffer.toString());
-    const updatedPackageJson = { ...packageJson, version: `${packageJson.version}+${buildMetadata}` };
+    const updatedPackageJson = { ...packageJson, version: `${packageJson.version}-${prerelease}` };
     await writeFile(packageJsonPath, JSON.stringify(updatedPackageJson, null, 2).concat(`\n`));
   }
 };
