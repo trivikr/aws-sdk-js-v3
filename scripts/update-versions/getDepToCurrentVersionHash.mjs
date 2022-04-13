ce55157a6a965f6cdc6e08d0b4df9dd110aae99a
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-04-13 19:54:08 +0000

    chore(scripts): use name and version from package.json in getDepToCurrentVersionHash

--- scripts/update-versions/getDepToCurrentVersionHash.mjs
+++ scripts/update-versions/getDepToCurrentVersionHash.mjs
@@ -1,15 +1,12 @@
 // @ts-check
 import { readFileSync } from "fs";
-import { basename, join } from "path";
+import { join } from "path";
 
 import { getWorkspacePaths } from "../utils/getWorkspacePaths.mjs";
 
 export const getDepToCurrentVersionHash = () =>
   getWorkspacePaths().reduce((acc, workspacePath) => {
     const packageJsonPath = join(workspacePath, "package.json");
-    const packageJson = JSON.parse(readFileSync(packageJsonPath).toString());
-    return {
-      ...acc,
-      [`@aws-sdk/${basename(workspacePath)}`]: packageJson.version,
-    };
+    const { name, version } = JSON.parse(readFileSync(packageJsonPath).toString());
+    return { ...acc, [`@aws-sdk/${name}`]: version };
   }, {});
