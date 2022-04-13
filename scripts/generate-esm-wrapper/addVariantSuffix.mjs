82c5bcf6b6d3d25a5299d87cb8a4cc98c103efe0
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-04-13 20:13:29 +0000

    chore(scripts): add script addVariantSuffix

---
+++ scripts/generate-esm-wrapper/addVariantSuffix.mjs
@@ -0,0 +1,30 @@
+import { readFile, writeFile } from "fs/promises";
+import { basename, join } from "path";
+
+const getUpdatedDeps = (deps, packageNamesToAddSuffix, variantSuffix) =>
+  Object.fromEntries(
+    Object.entries(deps).map(([dep, version]) => [
+      packageNamesToAddSuffix.includes(dep) ? `${dep}-${variantSuffix}` : dep,
+      version,
+    ])
+  );
+
+export const addVariantSuffix = async (workspacePaths, variantSuffix) => {
+  const packageNamesToAddSuffix = workspacePaths.map(basename);
+  for (const workspacePath of workspacePaths) {
+    const packageJsonPath = join(workspacePath, "package.json");
+    const packageJsonBuffer = await readFile(packageJsonPath);
+    const packageJson = JSON.parse(packageJsonBuffer.toString());
+    const updatedPackageJson = {
+      ...packageJson,
+      name: `${packageJson.name}-${variantSuffix}`,
+      ...(packageJson.dependencies && {
+        dependencies: getUpdatedDeps(packageJson.dependencies, packageNamesToAddSuffix, variantSuffix),
+      }),
+      ...(packageJson.devDependencies && {
+        devDependencies: getUpdatedDeps(packageJson.devDependencies, packageNamesToAddSuffix, variantSuffix),
+      }),
+    };
+    await writeFile(packageJsonPath, JSON.stringify(updatedPackageJson, null, 2).concat(`\n`));
+  }
+};
--- scripts/generate-esm-wrapper/index.mjs
+++ scripts/generate-esm-wrapper/index.mjs
@@ -4,33 +4,23 @@ import { join } from "path";
 import { getDepToCurrentVersionHash } from "../update-versions/getDepToCurrentVersionHash.mjs";
 import { updateVersions } from "../update-versions/updateVersions.mjs";
 import { getWorkspacePaths } from "../utils/getWorkspacePaths.mjs";
-import { addPreReleaseVersionSuffix } from "./addPreReleaseVersionSuffix.mjs";
-import { deleteFilesWithExtension } from "./deleteFilesWithExtension.mjs";
-import { deleteNotNodeDeps } from "./deleteNotNodeDeps.mjs";
-import { deleteNotNodeEntriesInPackageJson } from "./deleteNotNodeEntriesInPackageJson.mjs";
+import { addVariantSuffix } from "./addVariantSuffix.mjs";
 import { renameOrgInPackageName } from "./renameOrgInPackageName.mjs";
-import { replaceMarkdown } from "./replaceMarkdown.mjs";
 import { updateFilesInPackageJson } from "./updateFilesInPackageJson.mjs";
 
 // In release automation, the steps need to be run for all workspace just once.
 // After that the steps needs to be only for the packages which need to be published.
+// List of all workspaces which ship node artifacts is needed for addVariantSuffix though.
 const distFolderName = "dist-cjs";
 const workspacePaths = getWorkspacePaths().filter((workspacePath) => existsSync(join(workspacePath, distFolderName)));
-const prerelease = "node.cjs";
 
-await addPreReleaseVersionSuffix(workspacePaths, prerelease);
+// The variant suffix is not needed in production script.
+// This is added for testing publishined packahes with `@trivikr-test` org.
+await addVariantSuffix(workspacePaths, "esm");
 
 updateVersions(getDepToCurrentVersionHash());
 
-await deleteNotNodeEntriesInPackageJson(workspacePaths);
-
-await deleteFilesWithExtension(workspacePaths, distFolderName, "*.browser.js");
-await deleteFilesWithExtension(workspacePaths, distFolderName, "*.native.js");
-
 await updateFilesInPackageJson(workspacePaths, [distFolderName]);
-
-await deleteNotNodeDeps(workspacePaths);
-await replaceMarkdown(workspacePaths);
 
 // Renaming org in package name is not needed in production script.
 // This is added for testing published packages with `@trivikr-test` org.
