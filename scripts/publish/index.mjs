0d584e1d0b861f6f11d3572f9e0333cbdf7f3f76
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2022-04-13 14:49:49 -0700

    chore(scripts): add temporary script to publish on npm (#333)
    * chore(scripts): add temporary script to publish on npm
    
    * fix(scripts): publish without tags
    
    * chore(scripts): print npmPublishStatus after publish succeeds

---
+++ scripts/publish/index.mjs
@@ -0,0 +1,47 @@
+import { exec } from "child_process";
+import { readFile } from "fs/promises";
+import { join } from "path";
+import { promisify } from "util";
+
+import { getWorkspacePaths } from "../utils/getWorkspacePaths.mjs";
+
+const execPromise = promisify(exec);
+
+const workspacePaths = getWorkspacePaths();
+
+const npmPublishStatus = {};
+
+// All workspaces need to be published just once.
+// The release automation should publish only the changed workspaces.
+for (const workspacePath of workspacePaths) {
+  const packageJsonPath = join(workspacePath, "package.json");
+  const packageJsonBuffer = await readFile(packageJsonPath);
+  const packageJson = JSON.parse(packageJsonBuffer.toString());
+
+  // Skip publishing private packages.
+  if (packageJson?.private === true) {
+    continue;
+  }
+
+  const packageNameWithVersion = `${packageJson.name}@${packageJson.version}`;
+  // npm token is set in ~/.npmrc
+  try {
+    const response = await execPromise("npm publish", {
+      cwd: workspacePath,
+      env: {
+        npm_config_registry: "https://registry.npmjs.org/",
+        npm_config_access: "public",
+        PATH: process.env.PATH,
+      },
+    });
+    console.log(response);
+    npmPublishStatus[packageNameWithVersion] = "SUCCESS";
+  } catch (error) {
+    // Swallow error as this temporary script tries to publish over existing packages.
+    // The release automation will publish only the changed workspaces.
+    console.log(error);
+    npmPublishStatus[packageNameWithVersion] = "FAILURE";
+  }
+}
+
+console.log({ npmPublishStatus });
