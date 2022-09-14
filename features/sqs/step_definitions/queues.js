47e8c91766909c51f3c3273cdaec145d1ec09fbe
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-09-14 18:23:32 +0000

    chore: wait one second before using the queue

--- features/sqs/step_definitions/queues.js
+++ features/sqs/step_definitions/queues.js
@@ -6,6 +6,11 @@ Given("I create a queue with prefix {string}", async function (prefix) {
   const { QueueUrl } = await this.service.createQueue({ QueueName: name });
   this.queueUrl = QueueUrl;
   this.createdQueues.push(QueueUrl);
+
+  // After you create a queue, you must wait at least one second after the queue
+  // is created to be able to use the queue.
+  // https://docs.aws.amazon.com/AWSSimpleQueueService/latest/APIReference/API_CreateQueue.html
+  await new Promise((resolve) => setTimeout(resolve, 1000));
 });
 
 Then("list queues with prefix {string} should return queue urls", async function (prefix) {
