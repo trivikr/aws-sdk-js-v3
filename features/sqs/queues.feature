24b7dfd6522df6cfda74e8065092eb004a7c9aa8
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-09-14 17:41:45 +0000

    chore: use list queues with prefix

--- features/sqs/queues.feature
+++ features/sqs/queues.feature
@@ -5,6 +5,6 @@ Feature: SQS Queues
   I want to be able to create, list and delete queues.
 
   Scenario: Creating and deleting queues
-    Given I create a queue with the prefix name "aws-js-sdk"
-    And I create a queue with the prefix name "aws-js-sdk"
-    Then list queues should eventually return the queue urls
+    Given I create a queue with prefix "aws-js-sdk"
+    And I create a queue with prefix "aws-js-sdk"
+    Then list queues with prefix "aws-js-sdk" should return queue urls
--- features/sqs/step_definitions/queues.js
+++ features/sqs/step_definitions/queues.js
@@ -1,30 +1,16 @@
 const { Given, Then } = require("@cucumber/cucumber");
+const { equal } = require("assert");
 
-Given("I create a queue with the prefix name {string}", async function (prefix) {
+Given("I create a queue with prefix {string}", async function (prefix) {
   const name = this.uniqueName(prefix);
-  const response = await this.service.createQueue({ QueueName: name });
-  this.queueUrl = response.QueueUrl;
-  this.createdQueues.push(this.queueUrl);
+  const { QueueUrl } = await this.service.createQueue({ QueueName: name });
+  this.queueUrl = QueueUrl;
+  this.createdQueues.push(QueueUrl);
 });
 
-Then("list queues should eventually return the queue urls", function (callback) {
-  this.eventually(
-    callback,
-    function (next) {
-      next.condition = function () {
-        let matchingCount = 0;
-        for (let i = 0; i < this.createdQueues.length; ++i) {
-          for (let j = 0; j < this.data.QueueUrls.length; ++j) {
-            if (this.createdQueues[i] == this.data.QueueUrls[j]) {
-              matchingCount++;
-            }
-          }
-        }
-        return matchingCount == this.createdQueues.length;
-      };
-
-      this.request(null, "listQueues", {}, next);
-    },
-    { maxTime: 60 }
-  );
+Then("list queues with prefix {string} should return queue urls", async function (prefix) {
+  const { QueueUrls } = await this.service.listQueues({ QueueNamePrefix: prefix });
+  for (const queueUrl of this.createdQueues) {
+    equal(QueueUrls.includes(queueUrl), true);
+  }
 });
