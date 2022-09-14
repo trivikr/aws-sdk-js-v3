d55df51cad243afdb72f9c6a346a4967e7df4271
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-09-14 18:30:57 +0000

    chore: use eventually for list queues

--- features/sqs/step_definitions/queues.js
+++ features/sqs/step_definitions/queues.js
@@ -1,21 +1,30 @@
 const { Given, Then } = require("@cucumber/cucumber");
-const { equal } = require("assert");
 
 Given("I create a queue with prefix {string}", async function (prefix) {
   const name = this.uniqueName(prefix);
   const { QueueUrl } = await this.service.createQueue({ QueueName: name });
   this.queueUrl = QueueUrl;
   this.createdQueues.push(QueueUrl);
-
-  // After you create a queue, you must wait at least one second after the queue
-  // is created to be able to use the queue.
-  // https://docs.aws.amazon.com/AWSSimpleQueueService/latest/APIReference/API_CreateQueue.html
-  await new Promise((resolve) => setTimeout(resolve, 1000));
 });
 
-Then("list queues with prefix {string} should return queue urls", async function (prefix) {
-  const { QueueUrls } = await this.service.listQueues({ QueueNamePrefix: prefix });
-  for (const queueUrl of this.createdQueues) {
-    equal(QueueUrls.includes(queueUrl), true);
-  }
+Then("list queues with prefix {string} should return queue urls", function (prefix, callback) {
+  this.eventually(
+    callback,
+    function (next) {
+      next.condition = function () {
+        let matchingCount = 0;
+        for (let i = 0; i < this.createdQueues.length; ++i) {
+          for (let j = 0; j < this.data.QueueUrls.length; ++j) {
+            if (this.createdQueues[i] == this.data.QueueUrls[j]) {
+              matchingCount++;
+            }
+          }
+        }
+        return matchingCount == this.createdQueues.length;
+      };
+
+      this.request(null, "listQueues", { QueueNamePrefix: prefix }, next);
+    },
+    { maxTime: 60 }
+  );
 });
