7ea3595028d03e3d4e0008e93970be15c5e51dd8
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-09-21 03:42:00 +0000

    chore(scripts): remove tag from publish command

--- scripts/publish/index.mjs
+++ scripts/publish/index.mjs
@@ -1,6 +1,4 @@
 import { exec } from "child_process";
-import { readFile } from "fs/promises";
-import { join } from "path";
 import { promisify } from "util";
 
 import { getWorkspacePaths } from "../utils/getWorkspacePaths.mjs";
@@ -11,15 +9,8 @@ const workspacePaths = getWorkspacePaths();
 // All workspaces need to be published just once.
 // The release automation should publish only the changed workspaces.
 for (const workspacePath of workspacePaths) {
-  const packageJsonPath = join(workspacePath, "package.json");
-  const packageJsonBuffer = await readFile(packageJsonPath);
-  const { version } = JSON.parse(packageJsonBuffer.toString());
-  const tag = version.indexOf("-") > -1 ? version.substring(version.indexOf("-") + 1) : undefined;
-
-  // https://docs.npmjs.com/adding-dist-tags-to-packages
-  const npmPublishCommand = `npm publish${tag ? ` --tag ${tag}` : ``}`;
   // npm token is set in ~/.npmrc
-  const response = await execPromise(npmPublishCommand, {
+  const response = await execPromise(`npm publish`, {
     cwd: workspacePath,
     env: {
       npm_config_registry: "https://registry.npmjs.org/",
